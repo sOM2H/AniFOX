f48a5396b9edc26bd2e770c916bb6c2a8fe75dcb
Mikhail Melnyk <mikhail.melnyk@nixsolutions.com>
2019-11-15 17:43:02 +0200

    first commit

---
+++ app/helpers/video_reviews_helper.rb
@@ -0,0 +1,2 @@
+module VideoReviewsHelper
+end
---
+++ config/routes.rb
@@ -0,0 +1,7 @@
+Rails.application.routes.draw do
+
+  devise_for :admin_users, ActiveAdmin::Devise.config
+  ActiveAdmin.routes(self)
+  root "indexes#index"
+
+end
